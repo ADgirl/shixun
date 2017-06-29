2b6355eb34f6b4d32b8c9c4b5cfbfa7ee79f16f6
liuhongli <836948415@qq.com>
2017-06-29 21:27:30 +0800

    the eight class infomation

---
+++ class_eight/js/class_eight.js
@@ -0,0 +1,10 @@
+/*js实现图片描述的显示和隐藏*/
+	function show(){
+		var cover = document.getElementById('cover').style.display = 'block';
+	}
+	function hide(){
+		var cover = document.getElementById('cover').style.display = 'none'
+	}
+
+
+var banner = document.getElementById('banner');
