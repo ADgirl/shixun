55fad1556b04e31ac8002837694b87da07d427f1
liuhongli <836948415@qq.com>
2017-06-30 18:44:50 +0800

    add picture slide file and update my apple-iphone file

---
+++ class_nine/js/new_file.js
@@ -0,0 +1,31 @@
+var banner = document.getElementById('banner');
+var leftBtn = document.getElementById('left');
+var rightBtn = document.getElementById('right');
+var ul = banner.children[0].children[0];
+var target = 0;
+var leader = 0;
+	banner.onmouseover = function(){
+	 	leftBtn.style.display = 'block';
+	 	rightBtn.style.display = 'block';
+	}
+	banner.onmouseout = function(){
+		leftBtn.style.display = 'none';
+		rightBtn.style.display = 'none';
+	}
+	leftBtn.onclick = function(){
+		target -= banner.offsetWidth;
+		if(target <= -banner.offsetWidth * 4){
+			target = -banner.offsetWidth * 4
+		}
+	}
+	rightBtn.onclick = function(){
+		target += banner.offsetWidth;
+		if(target > 0){
+			target = 0;
+		}
+	}
+	setInterval(function(){
+		leader = leader+(target-leader)/10;
+		ul.style.left = leader +'px';
+	},10)
+
